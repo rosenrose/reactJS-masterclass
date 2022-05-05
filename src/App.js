c851db06d06c104693c6fbf6d276f6ead0c646bc
rosenrose <crazytempler@gmail.com>
2022-05-05 11:17:30 +0900

    styled component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import styled from "styled-components";
 
+const Parent = styled.div`
+  display: flex;
+`;
+const Box1 = styled.div`
+  background-color: tomato;
+  width: 10rem;
+  height: 10rem;
+`;
+const Box2 = styled.div`
+  background-color: teal;
+  width: 10rem;
+  height: 10rem;
+`;
+const Text = styled.h1`
+  color: white;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <Parent>
+      <Box1>
+        <Text>Hello</Text>
+      </Box1>
+      <Box2 />
+    </Parent>
+  );
 }
 
 export default App;

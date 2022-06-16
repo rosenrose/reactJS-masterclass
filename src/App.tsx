5179bfbc12d8589b7e9b79db473d2433db671e48
rosenrose <crazytempler@gmail.com>
2022-06-16 11:38:06 +0900

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ const Wrapper = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 1rem;
@@ -23,6 +22,7 @@ const Box = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 2rem;
 `;
 
 const boxVar = {
@@ -31,17 +31,69 @@ const boxVar = {
   exit: { opacity: 0, y: 20 },
 };
 
+const boxVar2 = {
+  initial: (direction: number) => ({ opacity: 0, scale: 0, x: 500 * direction }),
+  animate: { opacity: 1, scale: 1, x: 0, transition: { duration: 0.25 } },
+  exit: (direction: number) => ({
+    opacity: 0,
+    scale: 0,
+    x: -500 * direction,
+    rotateY: 360,
+    transition: { duration: 0.5 },
+  }),
+};
+
 const App = () => {
   const [isVisible, setIsVisible] = useState(false);
 
+  const [visibleIdx, setVisibleIdx] = useState(0);
+  const numbers = [...Array(5)].map((_, i) => i + 1);
+  const length = numbers.length;
+
+  const [direction, setDirection] = useState(1);
+  const scrollNext = () => {
+    setDirection(1);
+    setVisibleIdx((prev) => (prev + 1) % length);
+  };
+  const scrollPrev = () => {
+    setDirection(-1);
+    setVisibleIdx((prev) => (prev + length - 1) % length);
+  };
+
   return (
     <Wrapper>
+      <button onClick={() => setIsVisible((prev) => !prev)}>click</button>
       <AnimatePresence>
         {isVisible && (
           <Box variants={boxVar} {...boxVar} style={{ position: "absolute", top: "5rem" }} />
         )}
       </AnimatePresence>
-      <button onClick={() => setIsVisible((prev) => !prev)}>click</button>
+
+      <button onClick={scrollNext}>next</button>
+      <button onClick={scrollPrev}>prev</button>
+      <AnimatePresence custom={direction} exitBeforeEnter>
+        {/* {numbers.map((i) =>
+            i === visibleIdx + 1 ? (
+              <Box key={i} variants={boxVar2} {...boxVar2} style={{ position: "absolute" }}>
+                {i}
+              </Box>
+            ) : null
+          )}
+          key만 바뀌어도 리액트가 컴포넌트 생성, 삭제를 인식하여 다시 렌더링함
+          {...boxVar2}일 때 함수는 할당 불가능
+          */}
+        <Box
+          variants={boxVar2}
+          {...boxVar2}
+          initial="initial"
+          exit="exit"
+          custom={direction}
+          style={{ position: "absolute", bottom: "5rem" }}
+          key={visibleIdx}
+        >
+          {visibleIdx + 1}
+        </Box>
+      </AnimatePresence>
     </Wrapper>
   );
 };

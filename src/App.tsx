a8e65cb9f5f043d72d80205afd63a9d7b2598ab3
rosenrose <crazytempler@gmail.com>
2022-06-20 09:40:19 +0900

    layout animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
@@ -20,78 +20,38 @@ const Box = styled(motion.div)`
   background-color: #cdcdcd;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.7), 0 10px 20px rgba(0, 0, 0, 0.6);
   display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
 `;
 
-const boxVar = {
-  initial: { opacity: 0, scale: 0 },
-  animate: { opacity: 1, scale: 1, rotateZ: 360 },
-  exit: { opacity: 0, y: 20 },
-};
+const Box2 = styled(Box)`
+  justify-content: center;
+  align-items: center;
+`;
 
-const boxVar2 = {
-  initial: (direction: number) => ({ opacity: 0, scale: 0, x: 500 * direction }),
-  animate: { opacity: 1, scale: 1, x: 0, transition: { duration: 0.25 } },
-  exit: (direction: number) => ({
-    opacity: 0,
-    scale: 0,
-    x: -500 * direction,
-    rotateY: 360,
-    transition: { duration: 0.5 },
-  }),
-};
+const Circle = styled(motion.div)`
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+  background-color: #00a5ff;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.7), 0 5px 10px rgba(0, 0, 0, 0.6);
+`;
 
 const App = () => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  const [visibleIdx, setVisibleIdx] = useState(0);
-  const numbers = [...Array(5)].map((_, i) => i + 1);
-  const length = numbers.length;
-
-  const [direction, setDirection] = useState(1);
-  const scrollNext = () => {
-    setDirection(1);
-    setVisibleIdx((prev) => (prev + 1) % length);
-  };
-  const scrollPrev = () => {
-    setDirection(-1);
-    setVisibleIdx((prev) => (prev + length - 1) % length);
-  };
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
-    <Wrapper>
-      <button onClick={() => setIsVisible((prev) => !prev)}>click</button>
-      <AnimatePresence>
-        {isVisible && <Box {...boxVar} style={{ position: "absolute", top: "5rem" }} />}
-      </AnimatePresence>
-
-      <button onClick={scrollNext}>next</button>
-      <button onClick={scrollPrev}>prev</button>
-      <AnimatePresence custom={direction} exitBeforeEnter>
-        {/* {numbers.map((i) =>
-            i === visibleIdx + 1 ? (
-              <Box key={i} {...boxVar2} style={{ position: "absolute" }}>
-                {i}
-              </Box>
-            ) : null
-          )}
-          key만 바뀌어도 리액트가 컴포넌트 생성, 삭제를 인식하여 다시 렌더링함
-          {...boxVar2}일 때 함수는 할당 불가능
-          */}
-        <Box
-          variants={boxVar2}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          custom={direction}
-          style={{ position: "absolute", bottom: "5rem" }}
-          key={visibleIdx}
-        >
-          {visibleIdx + 1}
-        </Box>
-      </AnimatePresence>
+    <Wrapper onClick={() => setIsClicked((prev) => !prev)}>
+      <Box
+        style={{
+          justifyContent: isClicked ? "center" : "flex-start",
+          alignItems: isClicked ? "center" : "flex-start",
+        }}
+      >
+        <Circle layout />
+      </Box>
+      <Box2>{isClicked && <Circle layoutId="circle" style={{ borderRadius: "50%" }} />}</Box2>
+      <Box2>
+        {!isClicked && <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />}
+      </Box2>
     </Wrapper>
   );
 };

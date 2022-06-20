71a4271c3dbef8320023a8051ba7fadf827414dc
rosenrose <crazytempler@gmail.com>
2022-06-20 10:52:53 +0900

    netflix clone init

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import App from "./App";
-import { darkTheme } from "./theme";
+import { theme } from "./theme";
 
 const GlobalStyle = createGlobalStyle`
   @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap");
@@ -72,10 +73,12 @@ const GlobalStyle = createGlobalStyle`
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.querySelector("#root")
 );
--- src/theme.ts
+++ src/theme.ts
@@ -17,3 +17,16 @@ export const lightTheme: DefaultTheme = {
   boardColor: "#DADFE9",
   cardColor: "white",
 };
+
+export const theme: DefaultTheme = {
+  red: "#E51013",
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2F2F2F",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#E5E5E5",
+  },
+};

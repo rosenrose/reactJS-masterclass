431eb9b46e855f524e1653f1bf3618264306a617
rosenrose <crazytempler@gmail.com>
2022-07-21 07:54:01 +0900

    framer motion

--- framer motion/App.tsx
+++ framer motion/App.tsx
@@ -6,9 +6,10 @@ const Wrapper = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1rem;
+  gap: 3rem;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
   position: relative;
 `;
@@ -27,7 +28,21 @@ const Grid = styled.div`
 const Box = styled(motion.div)`
   height: 15rem;
   border-radius: 2rem;
-  background-color: #cdcdcd;
+  background-color: rgba(255, 255, 255, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BoxVar = {
+  whileHover: { scale: 1.2, transition: { duration: 0.3 } },
+};
+
+const Circle = styled(motion.div)`
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+  background-color: #fff;
 `;
 
 const Overlay = styled(motion.div)`
@@ -47,21 +62,35 @@ const OverlayVar = {
 
 const App = () => {
   const [id, setId] = useState<null | string>(null);
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
     <Wrapper>
       <Grid>
         {[...Array(4)].map((_, i) => (
-          <Box key={i} layoutId={i.toString()} onClick={() => setId(i.toString())} />
+          <Box
+            {...BoxVar}
+            key={i}
+            layoutId={i.toString()}
+            onClick={() => setId(i.toString())}
+            style={{
+              originX: i % 2 === 0 ? 1 : 0,
+              originY: i < 2 ? 1 : 0,
+            }}
+          >
+            {i === 1 && !isClicked && <Circle layoutId="circle" />}
+            {i === 2 && isClicked && <Circle layoutId="circle" />}
+          </Box>
         ))}
       </Grid>
       <AnimatePresence>
         {id && (
           <Overlay {...OverlayVar} onClick={() => setId(null)}>
-            <Box style={{ width: "30rem" }} layoutId={id!} />
+            <Box style={{ width: "30rem", backgroundColor: "#fff" }} layoutId={id} />
           </Overlay>
         )}
       </AnimatePresence>
+      <button onClick={() => setIsClicked((prev) => !prev)}>Switch</button>
     </Wrapper>
   );
 };

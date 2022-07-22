8dc31b4710d07d00543880dfeca572f93bc9bbdc
rosenrose <crazytempler@gmail.com>
2022-07-22 13:27:26 +0900

    trello clone

---
+++ trello clone/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  textColor: "#f5f6fa",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "black",
+};
+
+export const lightTheme: DefaultTheme = {
+  textColor: "black",
+  bgColor: "whitesmoke",
+  boardColor: "#DADFE9",
+  cardColor: "white",
+};
